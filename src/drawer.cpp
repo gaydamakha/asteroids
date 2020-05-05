1cc3103f8e3173321134ba81ce87df9c2c0fda0c
Mikhail Gaydamakha <mikhail.gaydamakha@etu.unistra.fr>
2020-05-04 20:42:13 -0400

    Fix shape color drawing

--- src/drawer.cpp
+++ src/drawer.cpp
@@ -2,23 +2,6 @@
 #include <Utils.h>
 #include "interface/drawer.h"
 
-//TODO: unify it
-// void Drawer::drawPolygone(const Polygone &polygone, const Color &color) const
-// {
-// 	setDrawColor(color);
-// 	auto vertices = polygone.getVertices();
-
-// 	for (auto i = vertices.begin(); i < --vertices.end(); ++i)
-// 	{
-// 		auto v1 = **i;
-// 		auto v2 = **vertices.next(i);
-// 		drawLineF(v1.getX(), v1.getY(), v2.getX(), v2.getY());
-// 	}
-// 	auto last = **(--vertices.end());
-// 	auto first = **vertices.begin();
-// 	drawLineF(last.getX(), last.getY(), first.getX(), first.getY());
-// }
-
 void Drawer::drawShape(const std::shared_ptr<Shape> shape, const Color &color) const
 {
 	this->drawShape(Vec2d(0, 0), shape, color);
@@ -33,19 +16,18 @@ void Drawer::drawShape(const Vec2d &pos, const std::shared_ptr<Shape> shape, con
 	}
 	else if (std::shared_ptr<ShapeWithVertices> s = std::dynamic_pointer_cast<ShapeWithVertices>(shape))
 	{
-		setDrawColor(color);
 		auto &vertices = s->getVertices();
 		for (auto i = vertices.begin(); i < --vertices.end(); ++i)
 		{
 			auto v1 = **i + pos;
 			auto v2 = **vertices.next(i) + pos;
-			drawLineF(v1.getX(), v1.getY(), v2.getX(), v2.getY());
+			drawLineF(v1.getX(), v1.getY(), v2.getX(), v2.getY(), color);
 		}
 		if (s->isClosed())
 		{
 			auto last = **(--vertices.end());
 			auto first = **vertices.begin();
-			drawLineF(last.getX(), last.getY(), first.getX(), first.getY());
+			drawLineF(last.getX(), last.getY(), first.getX(), first.getY(), color);
 		}
 	}
 	else
